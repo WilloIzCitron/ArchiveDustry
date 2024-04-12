4ea7124024933bacd0a64ffae1a9e7e0f3dd9400
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-04-12 20:13:54 +0700

    yippee

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
 const lib = require("soundlib");
 
-Events.on(GameOverEvent, event => {
-    const mySound = lib.loadMusic("lose").play();
+Events.on(GameOverEvent, winner => {
+    if(Vars.state.rules.pvp){
+    if(winner.team().id == Vars.player.team().id){
+        const mySound2 = lib.loadMusic("win");
+        mySound2.play();
+    } else {
+        const mySound = lib.loadMusic("lose");
+        mySound.play();
+    }
+    } else {
+        const mySound = lib.loadMusic("lose");
+        mySound.play();
+    }
+    
+    Vars.ui.restart.hidden(() => Vars.tree.loadMusic("lose").stop());
+    Vars.ui.restart.hidden(() => Vars.tree.loadMusic("win").stop());
   })
 
 
