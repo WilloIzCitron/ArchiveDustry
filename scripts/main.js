b1ea10f2f3a1a080357b548e9527dee4f7acaf0e
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-04-12 16:44:02 +0700

    gaem over

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 const lib = require('soundlib');
 
-Events.on(SectorLoseEvent, event => {
+Events.on(GameOverEvent, event => {
     const mySound = lib.loadSound("lose");
     // engine will spawn this sound at this location (X,Y)
     mySound.at(1, 1);
   })
+
 
   // sector captured = win
 Events.on(SectorCaptureEvent, event => {
