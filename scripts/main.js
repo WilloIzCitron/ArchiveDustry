6a9484cf36e51b362660441d3a8540c9e6898f57
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-04-12 16:40:14 +0700

    winning and losing

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const lib = require('soundlib');
+
+Events.on(SectorLoseEvent, event => {
+    const mySound = lib.loadSound("lose");
+    // engine will spawn this sound at this location (X,Y)
+    mySound.at(1, 1);
+  })
+
+  // sector captured = win
+Events.on(SectorCaptureEvent, event => {
+    const mySound = lib.loadSound("win");
+    // engine will spawn this sound at this location (X,Y)
+    mySound.at(1, 1);
+  })
+  
