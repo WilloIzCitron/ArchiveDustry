c7186fe1330f31442814aaa919f6920826d087ba
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-04-13 16:06:16 +0700

    database and add game5

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,25 @@
 const lib = require("soundlib");
 
+Events.on(ClientLoadEvent, () => {
+    Vars.tree.loadMusic("research").setLooping(true);
+    Vars.tree.loadMusic("database").setLooping(true);
+    Vars.ui.research.shown(() => {
+        Vars.tree.loadMusic("research").play();
+    });
+    
+    Vars.ui.research.hidden(() => {
+        Vars.tree.loadMusic("research").stop();
+    });
+
+    Vars.ui.database.shown(() => {
+        Vars.tree.loadMusic("database").play();
+    });
+    
+    Vars.ui.database.hidden(() => {
+        Vars.tree.loadMusic("database").stop();
+    });
+})
+
 Events.on(GameOverEvent, winner => {
     if(Vars.state.rules.pvp){
     if(winner.team().id == Vars.player.team().id){
@@ -19,15 +39,6 @@ Events.on(GameOverEvent, winner => {
         Vars.tree.loadMusic("win").stop();
     });
   })
-
-Vars.tree.loadMusic("research").setLooping(true)
-Vars.ui.research.shown(() => {
-    Vars.tree.loadMusic("research").play()
-})
-
-Vars.ui.research.hidden(() => {
-    Vars.tree.loadMusic("research").stop()
-})
 
   // sector captured = win
 Events.on(SectorCaptureEvent, event => {
